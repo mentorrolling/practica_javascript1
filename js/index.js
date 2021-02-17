ea56517d15d90516ca8d6a72f2ee6872357eb6cc
Pablo Marino <pmarino2013@gmail.com>
2021-02-17 18:08:55 -0300

    Arreglando js para que el texto aparezca uno debajo del otro

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 //variables de los datos de la card
 
-let nombreCompleto = prompt('Ingrese su nombre y apellido')
-let edad = prompt('Ingrese su edad')
-let actividad = prompt('¿Qué te gusta hacer?')
-let deporte = prompt('¿Practicas algun deporte?')
+let nombreCompleto = prompt("Ingrese su nombre y apellido");
+let edad = prompt("Ingrese su edad");
+let actividad = prompt("¿Qué te gusta hacer?");
+let deporte = prompt("¿Practicas algun deporte?");
 
 // //div de la card
-let card = document.getElementById('container__card')
-cardContent = document.getElementById('card__input')
+let card = document.getElementById("container__card");
+cardContent = document.getElementById("card__input");
 
-let datos = document.createElement('div')
-datosUsuario = `Nombre: ${nombreCompleto} /
-Edad: ${edad} / 
-Actividad: ${actividad} /
-Deporte: ${deporte}
-`
+let datos = document.createElement("div");
+let datosUsuario = `<p>Nombre: ${nombreCompleto}</p>
+<p>Edad: ${edad}</p> 
+<p>Actividad: ${actividad}</p>
+<p>Deporte: ${deporte}</p>
+`;
 //acoplar datos al card
-datos.append(datosUsuario)
-cardContent.append(datos)
+datos.innerHTML = datosUsuario;
+cardContent.append(datos);
